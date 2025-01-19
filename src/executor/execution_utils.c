ec68d95d9ffde6e07bdb698a90317da55ecefc94
Elroy Andela <eandela@f0r3s9.codam.nl>
2025-01-19 18:53:27 +0100

    add error message and code (127) for nonexistant folders

--- src/executor/execution_utils.c
+++ src/executor/execution_utils.c
@@ -6,7 +6,7 @@
 /*   By: eandela <eandela@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/05 21:35:24 by eandela       #+#    #+#                 */
-/*   Updated: 2025/01/16 16:39:32 by eandela       ########   odam.nl         */
+/*   Updated: 2025/01/19 18:52:01 by eandela       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,16 +46,23 @@ void	check_if_directory(char *path, t_command *cmd_list)
 {
 	struct stat	path_stat;
 
-	if (!path)
-	{
-		error_command_not_found(cmd_list->args[0]);
-		exit(127);
-	}
 	if (stat(path, &path_stat) == 0 && S_ISDIR(path_stat.st_mode))
 	{
 		ft_putstr_fd("minishell: ", STDERR_FILENO);
 		ft_putstr_fd(cmd_list->args[0], STDERR_FILENO);
 		ft_putstr_fd(": is a directory\n", STDERR_FILENO);
 		exit(126);
+	}
+	if (ft_strchr(cmd_list->args[0], '/') && !path)
+	{
+		ft_putstr_fd("minishell: ", STDERR_FILENO);
+		ft_putstr_fd(cmd_list->args[0], STDERR_FILENO);
+		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
+		exit(127);
+	}
+	if (!path)
+	{
+		error_command_not_found(cmd_list->args[0]);
+		exit(127);
 	}
 }
